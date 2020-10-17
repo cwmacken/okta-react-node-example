bbd29eed3310cc97a452145cfe8d276b0af6201a
Conor Macken <>
2020-10-16 17:01:13 -0700

    first commit > boilerplate working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Security {...oktaConfig}>
+        <App />
+      </Security>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
@@ -15,3 +25,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) module.hot.accept();
